e9e992cd517fb1d488c99d935980e4aa2b6d2f36
Andrei Lapets <a@lapets.io>
2020-08-30 22:01:17 -0400

    Factor demo into a reusable base class and a derived class (for main demo app).

--- splash.js
+++ splash.js
@@ -1,101 +1,11 @@
-class Splash {
+class Splash extends Sample {
   /* Example application demonstrating use of the retroscapes library
      to render the interactive splash landscape.
    */
   constructor(canvasElementId) {
-    this.canvasElementId = canvasElementId;
+    super(canvasElementId);
 
-    this.feed = new retroscapes.Feed({
-      "quantity": 32, "d": 8,
-      "pAnchor": 0.03, "pTethered": 0.9
-    });
     this.night = false;
-    this.center = {"x": null, "y": null};
-    this.projection = null;
-  }
-
-  _geometry() {
-    return new retroscapes.Geometry({
-      "orientation": {"tilt": 30},
-      "unit": 30,
-    });
-  }
-
-  _determineCenter() {
-    const ps = new URLSearchParams(window.location.search);
-    const x = ps.get("x"), y = ps.get("y");
-    this.center = {
-      "x": (x != null) ? parseInt(x) : Math.floor(Math.random() * 10000),
-      "y": (y != null) ? parseInt(y) : Math.floor(Math.random() * 10000)
-    };
-    return this.center;
-  }
-
-  _getProjection() {
-    return this.projection;
-  }
-
-  _setProjection(projection) {
-    this.projection = projection;
-    this.render.setProjection(projection);
-  }
-
-  initialize() {
-    this.scapes = this._build();
-    this.projection = this._geometry();
-    const self = this;
-    this.canvas = new retroscapes.Canvas(
-      document.getElementById(this.canvasElementId),
-      function () { return self._getProjection(); },
-      function (center) { self._redraw(center); },
-      [
-        new retroscapes.InteractionDrag(),
-        new retroscapes.InteractionNudge(
-          function () { return self._getPlateCenter(); }
-        )
-      ]
-    );
-
-    this.render = new retroscapes.Render({
-      "canvas": document.getElementById(this.canvasElementId),
-      "feed": this.feed,
-      "projection": this._getProjection(),
-      "background": new retroscapes.Color([255, 255, 255]),
-      "light": {"top":20, "left":-15, "right":-60},
-      "effects": [
-        new retroscapes.EffectPlateSquare(
-          function () { return self._getProjection(); }
-        )
-      ],
-      "cache": true,
-      "precedence": false
-    });
-
-    this._draw();
-  }
-
-  _getPlateBounds() {
-    const vps = this.render.rendered;
-    var bounds = {"xMin": null, "xMax": null, "yMin": null, "yMax": null};
-    for (var i = 0; i < vps.length; i++) {
-      if (vps[i].v.coordinates.z == 0) {
-        const x = vps[i].p.coordinates.x, y = vps[i].p.coordinates.y;
-        bounds.xMin = (bounds.xMin == null) ? x : Math.min(x, bounds.xMin);
-        bounds.yMin = (bounds.yMin == null) ? y : Math.min(y, bounds.yMin);
-        bounds.xMax = (bounds.xMax == null) ? x : Math.max(x, bounds.xMax);
-        bounds.yMax = (bounds.yMax == null) ? y : Math.max(y, bounds.yMax);
-      }
-    }
-    return bounds;
-  }
-
-  _getPlateCenter() {
-    const bounds = this._getPlateBounds();
-    const proj = this.projection;
-    return {
-      "x": (bounds.xMin + bounds.xMax) / 2,
-      "y": ((bounds.yMin + bounds.yMax) / 2) - (0.75 * (proj.uY + proj.uZ))
-    };
   }
 
   _alignment(vps) {
@@ -219,44 +129,6 @@ class Splash {
     }
     logo.style.display = 'block';
     links.style.display = 'block';
-  }
-
-  reinitialize() {
-    const center = this._getProjection().getCenter();
-    this._setProjection(this._geometry());
-    const vps = this.render.render(center, this.scapes, this.feed);
-    this._alignment(vps);
-  }
-
-  _draw() {
-    const center = this._determineCenter();
-    document.getElementById(this.canvasElementId).style.opacity = 0;
-    document.getElementById('logo').style.opacity = 0;
-    document.getElementById('links').style.opacity = 0;
-    const vps = this.render.render(center, this.scapes, this.feed);
-    this._alignment(vps);
-    document.getElementById(this.canvasElementId).style.opacity = 1;
-    const self = this;
-    this.render.renderGradually(
-      center,
-      this.scapes,
-      this.feed,
-      function () {
-        document.getElementById("logo").style.opacity = 1;
-        document.getElementById("links").style.opacity = 1;
-        self.reinitialize();
-      }
-    );
-  }
-
-  _redraw(center) {
-    center = (center == null) ? this._getProjection().getCenter() : center;
-    this._setProjection(this._geometry());
-    const c = (center == null) ? this._determineCenter() : center;
-    const vps = this.render.render(c, this.scapes, this.feed);
-    this._alignment(vps);
-    this.render.render(center, this.scapes, this.feed);
-    this.reinitialize();
   }
 
   setUrl() {
